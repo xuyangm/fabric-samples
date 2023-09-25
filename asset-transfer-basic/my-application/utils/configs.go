da41524ecbd6ea6375487a2947bf566d16d02ee0
Xuyang Ma <xuyangm7@gmail.com>
2023-09-25 12:23:58 +0800

    Update config
    Signed-off-by: Xuyang Ma <xuyangm7@gmail.com>

--- asset-transfer-basic/my-application/utils/configs.go
+++ asset-transfer-basic/my-application/utils/configs.go
@@ -2,8 +2,12 @@ package utils
 
 const (
     // Reed-Solomon code with (n, k) parameters
-    N int = 9
-    K int = 6
-    StripeSize int = 4096 * 6 //bytes redis
-    BlockHeight string = "9"
+    N int = 6
+    K int = 3
+    L int = 3
+    StripeSize int = 4096 * 3 //bytes redis
+    NumOfSlots int = 16384
 )
+
+var MasterNodes = [3]string {"localhost:50052", "localhost:50053", "localhost:50054"}
+var Weights = [3]string {"100", "100", "100"}
